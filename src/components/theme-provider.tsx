e6ed26f318c5de9c176d30000a686d90ad969e9a
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-25 21:57:20 +0530

    Adding Dark and Light Mode feature using Shadcn Components

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ModeToggle } from "./ThemeToggle";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+      <ModeToggle />
+    </NextThemesProvider>
+  );
+}
